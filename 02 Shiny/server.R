db38fdf45f84829e03977b6fdd91f73e64441fa4
Ryan Wechter <ryan.wechter@utexas.edu>
2015-11-23 18:17:41 -0600

    #winning

--- 02 Shiny/server.R
+++ 02 Shiny/server.R
@@ -8,20 +8,27 @@ require(shinydashboard)
 require(leaflet)
 
 shinyServer(function(input, output) {
-  
-  df3 <- eventReactive(input$clicks3, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-        """select CAUSE_NAME, STATE, YEAR, DEATHS, AADR 
-          from COD c join NHCE n
-          on c.STATE = n.STATE_NAME;"""
-        ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_riw223', PASS='orcl_riw223', 
-        MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-  })
-  
-  output$distPlot3 <- renderPlot(height=1000, width=2000, {
-    plot3 <- ggplot(aes(x = DEATHS, y = Y1999)) +
-      geom_point() + geom_smooth(method = lm) + 
-      labs(title= 'Correlation between Spending on Hospital Care and Deaths caused by Diseases of the Heart', x= 'Deaths',y='Spending in 1999')
-    plot3
-  })
-  
+
+
+# Begin code for Second Tab:
+
+      df2 <- eventReactive(input$clicks2, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+            "select YEAR, ITEM, SPENDING from MELTEDDATA"
+            ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_riw223', PASS='orcl_riw223', 
+            MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+      })
+
+      output$distPlot2 <- renderPlot(height=1000, width=2000, {
+            plot1 <- ggplot () + 
+              coord_cartesian() +
+              scale_x_discrete() +
+              scale_y_continuous() +
+              labs(title = 'Annual Healthcare Expenditure in Texas by Service Type',x='Year',y='Spending') +
+              layer(data = df2(), 
+                    mapping = aes(x = YEAR, y =SPENDING, fill = ITEM), 
+                    stat = 'identity',
+                    geom = 'bar') + 
+              theme(axis.text.x = element_text(angle = 45,size = 10))
+              plot1
+      })
 })
--- 02 Shiny/ui.R
+++ 02 Shiny/ui.R
@@ -3,12 +3,39 @@
 library(shiny)
 
 navbarPage(
-  title = "Data Visualization",
-  tabPanel( title = "Scatterplot",
-    sidebarPanel(
-      actionButton(inputId = "clicks3", label = "Click Me")
-      ),
-      mainPanel(plotOutput("distplot3")
-      )
-    )
+  title = "Elements of Visualization",
+  tabPanel(title = "Crosstab",
+     sidebarPanel(
+       actionButton(inputId = "light", label = "Light"),
+       actionButton(inputId = "dark", label = "Dark"),
+       sliderInput("KPI1", "KPI_Low_Max_value:", 
+                   min = 1, max = 7,  value = 7),
+       sliderInput("KPI2", "KPI_Medium_Max_value:", 
+                   min = 7, max = 10,  value = 10),
+       textInput(inputId = "title", 
+                 label = "Crosstab Title",
+                 value = "Diamonds Crosstab\nSUM_PRICE, SUM_CARAT, SUM_PRICE / SUM_CARAT"),
+       actionButton(inputId = "clicks1",  label = "Click me")
+     ),
+     
+     mainPanel(plotOutput("distPlot1")
+     )
+  ),
+  tabPanel(title = "Barchart",
+     sidebarPanel(
+       actionButton(inputId = "clicks2",  label = "Click me")
+     ),
+     
+     mainPanel(plotOutput("distPlot2")
+     )
+  ),
+  tabPanel(title = "ScatterPlot",
+     sidebarPanel(
+       actionButton(inputId = "clicks3",  label = "Click me")
+     ),
+     
+     mainPanel(plotOutput("distPlot3")
+     )        
   )
+)
+  
