6909be99f3f967f3cf74e36779f9e81efaa9fc07
Ryan Wechter <ryan.wechter@utexas.edu>
2015-11-23 18:29:16 -0600

    Dont break please

--- 02 Shiny/Crosstab/server.R
+++ 02 Shiny/Crosstab/server.R
@@ -49,5 +49,22 @@ shinyServer(function(input, output) {
             print(as.numeric(input$clicks))
       })
 
-
+      df2 <- eventReactive(input$clicks2, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="select YEAR, ITEM, SPENDING from MELTEDDATA"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+      })
+      
+      output$distPlot2 <- renderPlot(height=1000, width=2000, {
+        plot1 <- ggplot () + 
+          coord_cartesian() +
+          scale_x_discrete() +
+          scale_y_continuous() +
+          labs(title = 'Annual Healthcare Expenditure in Texas by Service Type',x='Year',y='Spending') +
+          layer(data = df2(), 
+                mapping = aes(x = YEAR, y =SPENDING, fill = ITEM), 
+                stat = 'identity',
+                geom = 'bar') + 
+          theme(axis.text.x = element_text(angle = 45,size = 10))
+        plot1
+      })
+      
+      
 })
